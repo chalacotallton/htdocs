ae95722774586a2ffc31b1938fe05fd3a0d882d8
T C Lacerda S <talltonchalaco@gmail.com>
2019-08-29 15:25:12 -0300

    first commit, added initial files

---
+++ first1.php
@@ -0,0 +1,12 @@
+<head>
+  <title> my first php file </title>
+</head>
+<body>
+  <p>
+    This is the first paragraph of my first php file. Although I have already written .htm files back in 2009, this is the first time I write a .php file.
+  </p>
+  <?php
+  echo 'this is my first line written inside a php tag';
+  echo 'I\'ll be back'
+  ?>
+</body>
---
+++ index1.php
@@ -0,0 +1,4 @@
+<h1>Hello from my first web page </h1>
+<p>
+  <a href="first1.php"> Click here to continue </a>
+</p>
