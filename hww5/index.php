7aa7c8e6ca2d2c4f9873b20c4763d1299263cb03
T C Lacerda S <talltonchalaco@gmail.com>
2019-09-03 22:34:22 -0300

    done homework 5, all good

---
+++ hww5/index.php
@@ -0,0 +1,25 @@
+<head>
+  <title> Tallton Chalaco Lacerda Santos</title>
+</head>
+<body>
+  <h1>Tallton Chalaco Lacerda Santos </h1>
+  <p> The SHA256 hash for "Tallton Chalaco Lacerda Santos" is
+  <?php
+    print hash('sha256', 'Tallton Chalaco Lacerda Santos');
+  ?>
+  </p>
+  <pre>
+ASCII ART:
+
+    ****************
+    **    ***     **
+          ***
+          ***
+          ***
+          ***
+  </pre>
+  <p>
+    <a href="check.php"> Click here to check the error setting</a><br>
+    <a href="fail.php"> Click here to cause a traceback</a>
+  </p>
+</body>
