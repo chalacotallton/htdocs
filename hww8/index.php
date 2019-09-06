b79aa897a42e6b2e6752b4f6404e989e46b26384
T C Lacerda S <talltonchalaco@gmail.com>
2019-09-06 17:27:57 -0300

    finished hww8

--- hww8/index.php
+++ hww8/index.php
@@ -1,2 +1,17 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>Tallton Chalaco Lacerda Santos Rock Paper Scissors d8930161 </title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
