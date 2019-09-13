809ecdf21115a371454753099e993ede0e862594
T C Lacerda S <talltonchalaco@gmail.com>
2019-09-13 16:37:43 -0300

    started session class

---
+++ Databasephp/cookies.php
@@ -0,0 +1,9 @@
+<?php
+if (! isset($_COOKIE['zap'])) {
+  setcookie('zap', '42', time()+3600);
+}
+?>
+<pre>
+  <?php print_r($_COOKIE); ?>
+</pre>
+<p><a href="cookies.php">Click Me!</a> or Press Refresh</p>
