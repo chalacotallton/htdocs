5d00857d147834fac931f93dee802c4051ccc342
T C Lacerda S <talltonchalaco@gmail.com>
2019-09-18 00:07:54 -0300

    javascript superbasics

---
+++ Javascript/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+    <p>one Paragraph</p>
+    <p><a href="js-03.htm" onclick="alert('Hi'); return true;">Next Page </a></p>
+    <p>Third Paragraph</p>
+    <noscript>
+      Whats going on
+    </noscript>
+    <p>Second Paragraph</p>
+  </body>
+  </html>
