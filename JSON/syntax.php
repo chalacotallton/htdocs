9c792e7cbd7f9b3820415c4ffc9fa454052f76c8
T C Lacerda S <talltonchalaco@gmail.com>
2019-09-21 00:40:03 -0300

    started studying json

---
+++ JSON/syntax.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+  <script type="text/text/javascript">
+  who = {
+    'name': 'Chuck', 'age': 29, 'college': true, 'offices': ['3350DMC', '3437NQ'], 'skills': {'fortran': 10, 'c': 10, 'c++': 5, 'python': '7' }
+  };
+  window.console && console.log(who);
+  </script>
+  
+</body>
+</html>
