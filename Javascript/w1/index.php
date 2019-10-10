a3ec3f8e1736a0a74a75dfdd3f67c166dc38afb0
T C Lacerda S <talltonchalaco@gmail.com>
2019-10-10 17:57:49 -0300

    finished static index.php's static part. need to add php

---
+++ Javascript/w1/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="w1.css">
+  <link href="https://fonts.googleapis.com/css?family=Turret+Road&display=swap" rel="stylesheet">
+  <title>TALLTON CHALACO LACERDA SANTOS ' Resume Registry </title>
+</head>
+<body>
+  <header>
+    <h1>CHALACO LACERDA'S Resume Registry</h1>
+  </header>
+  <main>
+    <a href=login.php>Please log in</a>
+  </main>
+  <?php
+
+
+  ?>
+</body>
+</html>
